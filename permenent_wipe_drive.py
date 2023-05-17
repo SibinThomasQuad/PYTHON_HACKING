7141b631dbf9c32dd275e1bd461d07560245088f
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2023-05-17 09:47:44 +0530

    Update permenent_wipe_drive.py

--- permenent_wipe_drive.py
+++ permenent_wipe_drive.py
@@ -6,6 +6,13 @@ import os
 import psutil
 
 class Drive:
+    def create_random_files(self,drive,size):
+        file_obj = File()
+        for i in range(size):
+            file_name = file_obj.generate_file_name()
+            file_path = str(drive)+'/'+file_name+"-"+str(i)
+            file_obj.generate_one_mb_text_file(file_path)
+    
     def list(self):
         drives = [ chr(x) + ":" for x in range(65,91) if os.path.exists(chr(x) + ":") ]
         return drives
@@ -17,6 +24,13 @@ class Drive:
         
     
 class File:
+    
+    def delete_existing_file(self,drive):
+        for path, subdirs, files in os.walk(drive):
+            for name in files:
+                file_with_path = os.path.join(path, name)
+                os.unlink(file_with_path)
+    
     def generate_file_name(self):
         now = datetime.datetime.now()
         milliseconds = now.microsecond // 1000  # Convert microseconds to milliseconds
@@ -48,6 +62,13 @@ def main():
     drive_size = drive_obj.get_size(str(drive_choosen))
     print("[+] You choosed "+str(drive_choosen)+" to permenent eraze")
     print("[+] Size "+str(drive_size))
-    #print(file_obj.generate_file_name())
-
+    rounded_size = rounded_integer = round(drive_size)
+    round_wipe = input("Enter round you want to replace > ")
+    for i in range(int(round_wipe)):
+        print("[+] ROUND "+str(i)+" STARTED")
+        file_obj.delete_existing_file(drive_choosen)
+        drive_obj.create_random_files(drive_choosen,rounded_size)
+        file_obj.delete_existing_file(drive_choosen)
+        print("[+] ROUND "+str(i)+" COMPLETED")
+    
 main()
